9866dd8c48ec6dfa93bc5a07fe7f6c289cdb0f4f
tjoscht <thomas.joscht@uni-bamberg.de>
2020-11-13 13:13:29 +0100

    Initialer Commit

---
+++ DisableBayZeitSound.ps1
@@ -0,0 +1,41 @@
+﻿# DEACTIVATE SOUND OF BAYZEIT
+# BAYZEIT saves the current date as binary value in registry. If this date is not current date it plays sound!
+# Registry Date is HEX like "84 C5 33 01". This must be reverted to "01 33 C5 84" and then converted to Decimal (int) results in "20170116" which is YEARMONTHDAY as string.
+
+# Get registry key as byte array
+$key = "HKCU:\Software\Breitenbach Software Engineering\DMS\DMSW 1.0\Settings"
+$binDate = (Get-ItemProperty -Path $key -Name Date).Date;
+write-host "Old bin (dec) date: " $binDate;
+
+# must be reverted
+[array]::Reverse($binDate)
+
+# hex to dec
+$date = [Convert]::ToString("0x" + $binDate[0].ToString("X2") + $binDate[1].ToString("X2")  + $binDate[2].ToString("X2") + $binDate[3].ToString("X2"),10)
+write-host "Old date: " $date;
+
+# Determine new date
+$currentDate = Get-Date
+# Date as DEC like 20170116
+$newDate =  $currentDate.Year.ToString("0000") + $currentDate.Month.ToString("00") + $currentDate.Day.ToString("00")
+write-host "New date: " $newDate
+# Convert to Int32
+$newDateInt = [Convert]::ToInt32($newDate)
+
+# Convert new date to hex string
+#$newbinDate = [Convert]::ToString($newDate, 16)
+$newbinString = "{0:x8}" -f $newDateInt
+
+# Convert hex string to hex byte array
+$newbinHex = New-Object byte[] 4
+$newbinHex[0] = "0x" + $newbinString.substring(0,2);
+$newbinHex[1] = "0x" + $newbinString.substring(2,2);
+$newbinHex[2] = "0x" + $newbinString.substring(4,2);
+$newbinHex[3] = "0x" + $newbinString.substring(6,2);
+
+# must be reverted
+[array]::Reverse($newbinHex)
+
+# Write registry key as byte array
+write-host "New bin (dec) date: " $newbinHex;
+Set-ItemProperty -Path $key -Name Date -Value $newbinHex
---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Disable BayZeit Sound #
+Das Bayerische Zeitmanagementsystem (**BayZeit**) ist eine Anwendung, welche vom Landesamt für Finanzen (LfF) des Freistaats Bayern für die Zeiterfassung der Mitarbeiter im öffentlichen Dienst verwendet wird. Diese wird für gewöhnlich im Internet Explorer gestartet.
+
+Mit dem Programm **DisableBayZeitSound** kann der Sound, welcher beim Start der BayZeit Anwendung abgespielt wird, deaktiviert werden.
+
+## Funktionsweise ##
+
+Das Programm **DisableBayZeitSound** besteht aus einem einzigen Powershell-Skript. Dieses setzt einen Wert in der Registry welcher dazu führt, dass der Sound beim Start *an diesem Tag* nicht mehr abgespielt wird.
+
+Damit der Sound dauerhaft deaktiviert ist, muss das Powershell-Skript einmal täglich vor dem Start von BayZeit ausgeführt werden. 
+
+## Installation ##
+Zur automatisierten Ausführung des Skripts empfiehlt es sich, dieses lokal als regelmäßig Aufgabe einzuplanen. Dabei ist es meist ausreichend, wenn das Programm bei der Windows Anmeldung eines Nutzers ausgeführt führt. 
+
+Die nötigen Schritte sind:
+
+1. Kopiere die DisableBayZeitSound.ps1 an eine beliebige Stelle (z.B. nach %UserProfile%)
+2. Erstelle eine Aufgabe in der Aufgabenplanung mit folgenden Einstellungen:
+	- Unabhängig von der Benutzeranmeldung ausführen
+	- Trigger: Bei Anmeldung
+	- Aktion erstellen: 
+	- Aktion: Programm starten
+	- Skript: "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+	- Argumente: -windowstyle hidden -file "D:\Privat\DisableBayZeitSound.ps1"
+3. **FERTIG!** Erfreue dich über die Stille 😊
